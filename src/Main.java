bdd0cbfa3825e4f3e7ba7a65fbee3512c15fd670
iima23 <elmazouz.imane@gmail.com>
2024-06-24 18:29:37 +0100

    add function of bubble sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
+    public static void bubble(int[] tab) {
+        int n = tab.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (tab[j] > tab[j + 1]) {
+                    int temp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = temp;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] tab = {1, 2, 4, 7, 3, 2, 8};
+        bubble(tab);
+        System.out.println(Arrays.toString(tab));
+        
     }
 }
